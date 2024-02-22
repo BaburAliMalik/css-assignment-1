fa7dfa9352f95d6cc16d2a11618f12e3e7f9f77b
baburali <baburali1910@gmail.com>
2024-02-22 03:29:07 -0800

    jv file created

---
+++ app.js
@@ -0,0 +1,15 @@
+alert("Welcome to JS Land \n Happy Coding");
+
+// variables
+
+var firstName = "babur ali";
+var lastName = "malik";
+var age = 23;
+
+// console.log
+
+console.log(firstName + lastName + age)
+
+// text in console.log
+
+console.log('this is my ' + firstName + " " + lastName + ' and this is my age ' + " " +  age)
